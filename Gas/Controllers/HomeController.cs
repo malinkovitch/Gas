d95bf27695375e65f8a64a0426e821c55427d071
malinkovitch <malinkovitch@gmail.com>
2015-10-21 13:24:51 +0300

    first

---
+++ Gas/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Привет. Страница Home";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Окно эбаут.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Страничка контактов";
+
+            return View();
+        }
+
+        public ActionResult Mock()
+        {
+            return View();
+        }
+    }
+}
